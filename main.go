98d35c77ed7227b56bf2957ca5090e09205df414
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-12-09 00:44:27 +0330

    marshaling and unmarshaling

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 )
@@ -30,6 +32,17 @@ func main() {
 	fmt.Println("Hi")
 }
 
+// store a list of book as a handler
+// define handeleBooks function and then struct
+// finally a slice of books by var
+
+type book struct {
+	Isbn string `"json:isbn`
+	Name string `"json:name`
+}
+
+var bList []book = []book{}
+
 func handeleBooks(w http.ResponseWriter, r *http.Request) {
 	// it use for handling request so it must receive a request(w) and
 	// response writer and a pointer to request
@@ -38,7 +51,25 @@ func handeleBooks(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 
+		// in this method we want to return a list of books(book list)
+		// this action needs write method
+		// change write method inputs to slice of books by json.marshal
+		b, _ := json.Marshal(bList)
+		w.Write(b)
+		return
+
 	case "POST":
+		// post usually use for generate a resource
+		// so we need body
+		// what is the content in body request
+		// make this body to array of bites
+		newBook := book{}
+		b, _ := io.ReadAll(r.Body)
+		json.Unmarshal(b, &newBook)
+		// we define a varibale named newbook in tpe book
+		// and use its address
+
+	default:
 		// if method isnt post and get , return an error for us
 		w.WriteHeader(http.StatusBadRequest)
 		return
